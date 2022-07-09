0863b64f2609f0d7399d4f1750f5f07de772eeec
Elisee-des <esabidani@gmail.com>
2022-07-09 10:42:31 +0000

    petit design et affichage des articles

--- articles.php
+++ articles.php
@@ -1,5 +1,20 @@
 <?php
 
+//on va chercher les articles dans la base
+require_once "includes/connection.php";
+
+$sql = "SELECT * FROM `articles` ORDER BY `created_at` DESC";
+
+$requete = $db->query($sql);
+
+$articles = $requete->fetchAll();
+
+// echo "<pre>";
+
+// var_dump($articles);
+
+// echo "</pre>";
+
 $titre = "Liset des articles";
 
 @include "includes/hearder.php";
@@ -8,8 +23,22 @@ include "includes/navbar.php";
 
 ?>
 
-<p>Contenu de la page d'articles</p>
-
+<section>
+    <div class="card">
+    <h2 class="text-center">Liste des articles</h2><br><br>
+       <div class="container">
+       <?php foreach ($articles as $article) : ?>
+            <div class="card">
+                <article>
+                    <h3><?= $article["title"] ?></h3>
+                    <p><?= $article["created_at"] ?></p>
+                    <div><?= $article["content"] ?></div>
+                </article><br>
+            </div><br>
+        <?php endforeach; ?>
+    </div>
+       </div>
+</section>
 <?php
 include "includes/footer.php";
 ?>
--- includes/navbar.php
+++ includes/navbar.php
@@ -1,6 +1,7 @@
-<nav>
-    <h1>Titre</h1>
-    <ul>
+
+<br><h1 class="text-center">Mon petit Blog</h1>
+<nav class="nav justify-content-center">
+    <ul class="text-align">
         <li><a href="index.php">Accueil </a></li>
         <li><a href="">Contact</a></li>
         <li><a href="articles.php">Articles</a></li>
